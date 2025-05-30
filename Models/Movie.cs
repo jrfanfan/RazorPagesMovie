44d2df4f0af14de6526aa649f88a9c7d29c2b5f6
Jrfanfan <jrfanfan@yahoo.fr>
2025-05-30 09:08:05 -0400

    update

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -15,4 +15,9 @@ public class Movie
 
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
+    public string Rating { get; set; } = string.Empty;
 }
